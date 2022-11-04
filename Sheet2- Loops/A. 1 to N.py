a446fede2c274cc4ab679a52c28932a0aa8d6898
Mohammed Atef <106938379+MohammedAtef0@users.noreply.github.com>
2022-11-04 20:41:07 +0200

    Update A. 1 to N.py

--- Sheet2- Loops/A. 1 to N.py
+++ Sheet2- Loops/A. 1 to N.py
@@ -1,3 +1,12 @@
+#using for loop
 n = eval (input("Enter a number N:"))
 for i in range (1 , n+1):
     print(i)
+------------------------------------------------------------------------------------------
+#using While loop
+n = eval(input("Enter a number N:"))
+i = 1
+while n >= 1:
+    print(i)
+    i = i + 1
+    n = n - 1
