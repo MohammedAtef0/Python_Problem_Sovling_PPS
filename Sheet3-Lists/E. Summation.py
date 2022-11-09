e349662e4e0a69583e094018e43d996a051ec008
Mohammed Atef <106938379+MohammedAtef0@users.noreply.github.com>
2022-11-09 07:31:30 +0200

    Update E. Summation.py

--- Sheet3-Lists/E. Summation.py
+++ Sheet3-Lists/E. Summation.py
@@ -4,3 +4,13 @@ for i in range (n) :
     x = eval(input("Enter a number x:"))
     Sum+= x
 print("The absolutes summation = " , abs(Sum) , sep="")
+
+----------------------------------------------------------------------------------
+
+#other solution
+n = eval(input("Enter a number n:"))
+a = []
+for i in range (n):
+    x = eval(input("Enter the array values:"))
+    a.append(x)
+print("The absolutes summation = " , abs(sum(a)) , sep="")
