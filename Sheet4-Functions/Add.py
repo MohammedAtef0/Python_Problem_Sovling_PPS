ec8104bb46ffa71c7a40de3f6283d6322e6f24f4
Mohammed Atef <106938379+MohammedAtef0@users.noreply.github.com>
2022-11-19 18:47:00 +0200

    Create Add.py

---
+++ Sheet4-Functions/Add.py
@@ -0,0 +1,6 @@
+def summation(x , y ):
+    return x + y
+
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+print("The Summation of the two numbers =", summation(a, b), end='')
