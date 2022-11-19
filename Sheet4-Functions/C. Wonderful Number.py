5c9143360a0c9ac7c4638999b84990fe8380ec1b
Mohammed Atef <106938379+MohammedAtef0@users.noreply.github.com>
2022-11-19 21:10:57 +0200

    Create C. Wonderful Number.py

---
+++ Sheet4-Functions/C. Wonderful Number.py
@@ -0,0 +1,27 @@
+def isOdd(n):
+    if n % 2 != 0 :
+        return True
+    else:
+        return False
+#checking if the number is odd
+def BinaryConverting(n):
+    binary = []
+    while n != 0:
+        if n % 2 == 0 :
+            n = n / 2
+            binary.insert(0,0)
+        else:
+            n = (n-1) / 2
+            binary.insert(0,1)
+    rev_binary = binary[::-1]
+    if binary == rev_binary:
+        return True
+    else:
+        return False
+#checking if the binary representation is palindrome
+x = int(input("Enter a number to check if it's wonderful or not:"))
+if isOdd(x) == True and BinaryConverting(x) == True:
+    print("YES")
+else:
+    print("NO")
+#end of the main method
