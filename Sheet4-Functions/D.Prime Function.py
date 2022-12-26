9cb501d1594b489b9f53eff391e7e101fc83c5a1
Mohammed Atef <106938379+MohammedAtef0@users.noreply.github.com>
2022-12-26 21:22:28 +0200

    Update D.Prime Function.py

--- Sheet4-Functions/D.Prime Function.py
+++ Sheet4-Functions/D.Prime Function.py
@@ -14,3 +14,20 @@ for i in range (x):
         print("YES" )
     else:
         print("NO" )
+------------------------------------------------------------------------------------
+from math import sqrt
+def isPrime(n):
+    if n<= 1:
+        return False
+    for i in range (2,int(sqrt(n)+1)):
+        if n % i == 0 :
+            return False
+    return True
+#end of the isPrime Function
+x = int(input())
+for i in range (x):
+    NumberToCheck = int(input())
+    if isPrime(NumberToCheck) == True :
+        print("YES" )
+    else:
+        print("NO" )
