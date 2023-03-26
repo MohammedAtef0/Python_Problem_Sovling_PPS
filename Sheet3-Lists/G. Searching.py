46918e6d1bbba648e16a0245ae112f87fdd29c11
Mohammed Atef <106938379+MohammedAtef0@users.noreply.github.com>
2023-03-26 09:33:02 +0200

    Update G. Searching.py

--- Sheet3-Lists/G. Searching.py
+++ Sheet3-Lists/G. Searching.py
@@ -12,3 +12,24 @@ for i in range (n):
         c = c + 1
 if c == n:
     print(-1)
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Other solution 1
+x = int(input("Enter a N:"))
+a= []
+for i in range(x):
+    Lvalues = int(input("Enter list items:"))
+    a.append(Lvalues)
+y = int(input("Enter a number to search in the list:"))
+if y in a :
+    print(a.index(y))
+else:
+    print(-1)
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Other solution 2
+x = int(input("Enter a N:"))
+a = list(map(int, input("Enter the list values:").split()))
+y = int(input("Enter a number to search in the list:"))
+if y in a :
+    print(a.index(y))
+else:
+    print(-1)
