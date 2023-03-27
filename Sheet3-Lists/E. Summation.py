62927638c02dfc4e296c82db5e0db89ff412f3a0
Mohammed Atef <106938379+MohammedAtef0@users.noreply.github.com>
2023-03-27 13:57:27 +0200

    Update E. Summation.py

--- Sheet3-Lists/E. Summation.py
+++ Sheet3-Lists/E. Summation.py
@@ -14,3 +14,23 @@ for i in range (n):
     x = eval(input("Enter the array values:"))
     a.append(x)
 print("The absolutes summation = " , abs(sum(a)) , sep="")
+----------------------------------------------------------------------------------
+#other solution2
+n = int(input("Enter N:"))
+a,b,c = list(map(int,input("Enter list item's:").split()))
+print(abs(sum(a)))
+----------------------------------------------------------------------------------
+#other solution3
+#Taking from the user N
+n = int(input("Enter N:"))
+#initilaze empty list
+a=[];Sum=0
+#taking from the user list items
+for i in range(n):
+    x = int(input("Enter list item's:"))
+    a.append(x)
+    Sum += x
+if Sum > 0 :
+    print(Sum)
+else:
+    print(Sum * -1)
