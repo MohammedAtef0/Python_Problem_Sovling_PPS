c96af563f44ce8d27e688460152e0c1757d5717c
Mohammed Atef <106938379+MohammedAtef0@users.noreply.github.com>
2023-07-23 15:41:01 +0300

    Update 6. Sixth Problem.py

--- Sheet3-Lists/Extra Exercises/6. Sixth Problem.py
+++ Sheet3-Lists/Extra Exercises/6. Sixth Problem.py
@@ -1,7 +1,8 @@
+from math import *
 #6. Write a program that takes in a list of numbers and finds the largest prime number in the list.(The problem)
 
 #initializing an empty list 
-ListOfNumbers = []
+ListOfNumbers = [] ; ListOfPrimeNumbers =[]; IsPrime= False
 
 #taking from the user the ListOfNumber values 
 while True:
@@ -11,7 +12,23 @@ while True:
     else:
         ListOfNumbers.append(x)
 
-#putting the largest number in the list in MaxNumber variable 
-ListOfNumbers.sort()
+#checking for all prime numbers in the ListOfNumbers and append all prime numbers in a new list named ListOfPrimeNumbers
+for i in ListOfNumbers:
+    if i <= 1:
+        continue
+    for j in range(2 , int(sqrt(i) )+1):
+        if i % j == 0 :
+            break  
+    else:
+        ListOfPrimeNumbers.append(i)
 
-for i in range(sqrt()):
+#sorting the ListOfPrimeNumbers to get the last value in the list because it's the largest ListOfPrimeNumbers
+ListOfPrimeNumbers.sort()
+
+#checking if the list is empty or not!
+if len(ListOfNumbers) == 0 or len(ListOfPrimeNumbers) == 0:
+    print("The list is empty please try again!")
+else:
+    print("The largest prime number in list is:" , ListOfPrimeNumbers[-1])
+
+        
