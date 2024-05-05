548874a89a7b9e12a11586fb8e5e3dcd4af9279c
Mohammed Atef <106938379+MohammedAtef0@users.noreply.github.com>
2024-05-05 04:03:51 +0300

    Update J. Palindrome.py

--- Sheet2- Loops/J. Palindrome.py
+++ Sheet2- Loops/J. Palindrome.py
@@ -1,3 +1,4 @@
+# First Solution
 s = input("Enter a String:")
 String_Count= 0 ; String_Rev = -1 ; c = 0
 for i in range (len(s)):
@@ -7,3 +8,10 @@ if c == len(s):
     print("Yes")
 else:
     print("No")
+# =================================================================================================================================================================
+# Second Solution
+InputString = input("Enter your string to check if it is a palindrome string or not:")
+if InputString == InputString[::-1]:
+    print("YES")
+else:
+    print("NO")
