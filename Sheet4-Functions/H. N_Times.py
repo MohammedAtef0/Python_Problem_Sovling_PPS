99d8e74443eced4e51b43b0dc3690e735bb1d309
Mohammed Atef <106938379+MohammedAtef0@users.noreply.github.com>
2024-08-04 10:36:27 +0300

    Update H. N_Times.py

--- Sheet4-Functions/H. N_Times.py
+++ Sheet4-Functions/H. N_Times.py
@@ -8,3 +8,15 @@ for i in range (t):
     n = int(input("Enter the number to repeat the character:"))
     ch = input("Enter a character:")
     nTimes(n,ch)
+# ============================================================================
+# Other Solution 
+def nTimes(value,times):
+    for i in range(times):
+        print(value, end= ' ')
+    print()
+NumberofItems = int(input())
+for i in range(NumberofItems):
+    t,v = map(str,input().split())
+    t = int(t)
+    nTimes(v,t)
+# ============================================================================
