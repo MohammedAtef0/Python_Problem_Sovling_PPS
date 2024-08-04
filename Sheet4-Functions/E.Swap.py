606a5d6892a310290fbd9bc216c070d1c62e8d57
Mohammed Atef <106938379+MohammedAtef0@users.noreply.github.com>
2024-08-04 10:15:21 +0300

    Update E.Swap.py

--- Sheet4-Functions/E.Swap.py
+++ Sheet4-Functions/E.Swap.py
@@ -4,3 +4,10 @@ def Swap(x , y ):
 a = int(input("Enter the first number:"))
 b = int(input("Enter the second number:"))
 Swap(a , b )
+# ===============================================================
+# Other Solution
+def swap(x,y):
+    return y,x 
+a,b = map(int,input().split())
+print(*swap(a,b))
+===============================================================
