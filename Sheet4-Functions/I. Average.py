24f7f531fa6fc5173d5175d55b3873a8cdd2e3a2
Mohammed Atef <106938379+MohammedAtef0@users.noreply.github.com>
2024-12-24 11:48:04 +0200

    Update I. Average.py

--- Sheet4-Functions/I. Average.py
+++ Sheet4-Functions/I. Average.py
@@ -10,3 +10,15 @@ for i in range (n):
     x = eval(input())
     a.append(x)
 print(format(avg(n,a), '.7f'))
+# =============================================================================================================================
+# Avg Func.
+def Avg(n,a):
+    Sum = 0 
+    for i in a:
+        Sum += i
+    return Sum / n 
+# Input
+n = int(input())
+a = list(map(float,input().split()))
+# Print with the 7th decimal point
+print(format(Avg(n,a), '.7f'))
