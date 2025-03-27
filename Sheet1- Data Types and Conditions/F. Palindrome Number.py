00622b4306662a1cc41e576e1dd46590715dc018
Mohammed Atef <106938379+MohammedAtef0@users.noreply.github.com>
2025-03-27 23:40:38 +0200

    Update F. Palindrome Number.py

--- Sheet1- Data Types and Conditions/F. Palindrome Number.py
+++ Sheet1- Data Types and Conditions/F. Palindrome Number.py
@@ -5,21 +5,21 @@ if InputString == InputString[::-1]:
 else:
     print("NO")
 -------------------------------------------------------------------------------------------------------------
-#1st solution
+#1st solution This code works only if the numbers are from 0 to 100
 x = eval (input("Enter a number to check if it Palindrome Number or not:"))
 if x % 11 == 0:
     print("YES")
 else:
     print("NO")
 -------------------------------------------------------------------------------------------------------------
-#2nd solution
+#2nd solution This code works only if the numbers are from 0 to 100
 x =input("Enter a number to check if it Palindrome Number or not:")
 if x[0] == x[-1]:
     print("YES")
 else:
     print("NO")
 -------------------------------------------------------------------------------------------------------------    
-#3rd solution
+#3rd solution This code works only if the numbers are from 0 to 100
 x =eval(input("Enter a number to check if it Palindrome Number or not:"))
 if (x // 10) == (x % 10) :
     print("YES")
